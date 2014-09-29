f217010af971f5de01e3b2854e4c567b0c780700
gaoyukid <gaoyu.kid@gmail.com>
2014-09-29 21:38:08 +0800

    change the document root from hard-coded to arg

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Dserver.port=8888 -jar target/HtmlParser-0.0.1-SNAPSHOT.jar
+java -Dserver.port=8888 -Dweb.document=/Users/yug/Documents/procurement/template -jar target/HtmlParser-0.0.1-SNAPSHOT.jar
