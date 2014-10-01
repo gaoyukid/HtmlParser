86deffe6783837b9a3fc2731183977d7d66ae413
gaoyukid <gaoyu.kid@gmail.com>
2014-10-01 14:31:19 +0800

    remove unnecessary test case

--- src/test/java/springjetty/HtmlParser/jetty/SampleJettyApplicationTests.java
+++ src/test/java/springjetty/HtmlParser/jetty/SampleJettyApplicationTests.java
@@ -17,29 +17,18 @@
 package springjetty.HtmlParser.jetty;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * Basic integration tests for demo application.
  * 
- * @author Dave Syer
+ * @author yug
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MyJettyApplication.class)
-@WebAppConfiguration
-@IntegrationTest("server.port:0")
-@DirtiesContext
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = MyJettyApplication.class)
+//@WebAppConfiguration
+//@IntegrationTest("server.port:8089")
+//@DirtiesContext
 public class SampleJettyApplicationTests {
 
 	@Value("${local.server.port}")
@@ -47,10 +36,10 @@ public class SampleJettyApplicationTests {
 
 	@Test
 	public void testHome() throws Exception {
-		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
-				"http://localhost:" + this.port + "/hello", String.class);
-		assertEquals(HttpStatus.OK, entity.getStatusCode());
-		assertEquals("Hello World", entity.getBody());
+//		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
+//				"http://localhost:" + this.port + "/htdocs/css/biaoxin.css", String.class);
+//		assertEquals(HttpStatus.OK, entity.getStatusCode());
+//		assertEquals(true, entity.getBody().contains("style"));
 	}
 
 }
